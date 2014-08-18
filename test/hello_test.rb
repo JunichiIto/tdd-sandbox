c3207bb3c0878deeef3bb030375334538a9e9a83
Junichi Ito <jit@sonicgarden.jp>
2014-08-18 09:34:43 +0900

    Add test with power assert.

--- test/hello_test.rb
+++ test/hello_test.rb
@@ -4,4 +4,10 @@ class HelloTest < Test::Unit::TestCase
   def test_message_is_hello
     assert_equal "hello", Hello.new.message
   end
+
+  def test_message_is_hello_with_power_assert
+    assert do
+      Hello.new.message == "hello."
+    end
+  end
 end
